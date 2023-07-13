b112b1a5ecd3e987e891762b284c339a1b1116d2
Wyatt Ho <Wyatt@xing.com>
2023-07-13 18:02:55 +0800

    refactor

--- src/create_loop_rbe3.tcl
+++ src/create_loop_rbe3.tcl
@@ -1,12 +1,3 @@
-proc create_list_filled_with_value {length val} {
-    set answer ""
-    for {set i 0} {$i < $length} {incr i} {
-        lappend answer $val
-    }
-    return $answer
-}
-
-
 proc extrac_line_indices {loop_data} {
     # Parameters for the while loop
     set idx 2
@@ -61,6 +52,25 @@ proc check_target {mark_id dimension_x dimension_y} {
 }
 
 
+proc create_list_filled_with_value {length val} {
+    set answer ""
+    for {set i 0} {$i < $length} {incr i} {
+        lappend answer $val
+    }
+    return $answer
+}
+
+
+proc create_rbe3 {mark_id node_indices} {
+    set node_num [llength $node_indices]
+    set dofs [create_list_filled_with_value $node_num 123]
+    set weights [create_list_filled_with_value $node_num 1]
+    eval *createarray 23 $dofs
+    eval *createdoublearray 23 $weights
+    eval *rbe3 $mark_id 1 $node_num 1 $node_num 0 123456 1
+}
+
+
 # Select the target surface
 *createmarkpanel surfs 1 "Please select the target surface.."
 
@@ -81,20 +91,13 @@ set tgt_loop_count 0
 foreach loop_data $loops {
     set line_indices [extrac_line_indices $loop_data]
     set node_indices [extrac_node_indices $line_indices]
-    eval *createmark nodes 1 $node_indices
-    set is_target [check_target 1 $dimension_x $dimension_y]
-    
-    # Create RBE3
+    set mark_id 1
+    eval *createmark nodes $mark_id $node_indices
+    set is_target [check_target $mark_id $dimension_x $dimension_y]
     if {$is_target} {
-        puts $is_target
         incr tgt_loop_count 1
-        set node_num [llength $node_indices]
-        set dofs [create_list_filled_with_value $node_num 123]
-        set weights [create_list_filled_with_value $node_num 1]
-        eval *createarray 23 $dofs
-        eval *createdoublearray 23 $weights
-        eval *rbe3 1 1 $node_num 1 $node_num 0 123456 1
+        create_rbe3 $mark_id $node_indices
     }
-    *clearmark nodes 1
+    *clearmark nodes $mark_id
 }
 puts "Done! Created $tgt_loop_count RBE3s."
