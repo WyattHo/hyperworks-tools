b8db030b32af225e13dd32ab6ad5f644382e3caa
Wyatt Ho <Wyatt@xing.com>
2023-07-18 11:32:53 +0800

    fix: make the dof weights consistent with the node heights

--- src/create_mpc_between_rbe3s.tcl
+++ src/create_mpc_between_rbe3s.tcl
@@ -7,6 +7,18 @@ proc get_dependent_nodes {elem_indices} {
 }
 
 
+proc sort_nodes_in_z {node_pair} {
+    lassign $node_pair node1 node2
+    set coordz_1 [lindex [hm_getvalue nodes id=$node1 dataname=coordinates] end]
+    set coordz_2 [lindex [hm_getvalue nodes id=$node2 dataname=coordinates] end]
+    if {$coordz_1 > $coordz_2} {
+        return "$node1 $node2"
+    } else {
+        return "$node2 $node1"
+    }
+}
+
+
 proc find_rbe3_pairs {node_indices} {
     set rbe3_pairs {}
     set already_considered {}
@@ -23,7 +35,8 @@ proc find_rbe3_pairs {node_indices} {
         set distance_min [lindex [lsort $distances] 0]
         set idx_closest [lsearch $distances $distance_min]
         set node_idx_closest [lindex $other_indices $idx_closest]
-        lappend rbe3_pairs "$node_idx_current $node_idx_closest"
+        set node_pair [sort_nodes_in_z "$node_idx_current $node_idx_closest"]
+        lappend rbe3_pairs $node_pair
         lappend already_considered $node_idx_current $node_idx_closest
     }
     return $rbe3_pairs
@@ -47,8 +60,8 @@ proc create_mpc {independent_nodes dependent_node} {
     *createarray $dof_size [lrepeat $dof_size 4]
     *createdoublearray $weight_size [lrepeat $weight_size 1.0]
     *equationcreate $markid_nodes 1 $dof_size 1 $weight_size $dependent_node 3 1.0 0.0
-    *setvalue equations id=-1 STATUS=2 ROW=0 independentcoeffs3={1}
-    *setvalue equations id=-1 STATUS=2 ROW=1 independentcoeffs3={-1}
+    *setvalue equations id=-1 STATUS=2 ROW=0 independentcoeffs3={-1}
+    *setvalue equations id=-1 STATUS=2 ROW=1 independentcoeffs3={1}
     *clearmark nodes $markid_nodes
 }
 
