fb38a2c30d8d6a6b46f7e366e3ae04fbd2fdfb10
Wyatt Ho <Wyatt@xing.com>
2023-07-18 10:03:14 +0800

    feat: create mpc

--- src/create_mpc_between_rbe3s.tcl
+++ src/create_mpc_between_rbe3s.tcl
@@ -30,56 +30,40 @@ proc find_rbe3_pairs {node_indices} {
 }
 
 
+proc get_latest_node_id {} {
+    set markid_nodes 1
+    *createmark nodes $markid_nodes -1
+    set latest_node_id [hm_getmark nodes $markid_nodes]
+    *clearmark nodes $markid_nodes
+    return $latest_node_id
+}
+
+
+proc create_mpc {independent_nodes dependent_node} {
+    set markid_nodes 1
+    set dof_size [llength $independent_nodes]
+    set weight_size [expr 6 * $dof_size]
+    eval *createmark nodes $markid_nodes $independent_nodes
+    *createarray $dof_size [lrepeat $dof_size 4]
+    *createdoublearray $weight_size [lrepeat $weight_size 1.0]
+    *equationcreate $markid_nodes 1 $dof_size 1 $weight_size $dependent_node 3 1.0 0.0
+    # *setvalue equations id=1 STATUS=2 ROW=0 independentcoeffs3= {1}
+    # *setvalue equations id=1 STATUS=2 ROW=1 independentcoeffs3= {-1}
+}
+
+
 set markid_elems 1
 *createmarkpanel elems $markid_elems "Please select the target elements.."
 set elem_indices [hm_getmark elems $markid_elems]
 set dependent_nodes [get_dependent_nodes $elem_indices]
 set rbe3_pairs [find_rbe3_pairs $dependent_nodes]
 
+foreach node_pair $rbe3_pairs {
+    lassign $node_pair node1 node2
+    eval *createnodesbetweennodes $node1 $node2 1
+    set dependent_node [get_latest_node_id]
+    break
+    create_mpc $node_pair $dependent_node
+}
 
-# # NodeCr
-# *createnodesbetweenpositions -40.0000007 -60.0000027 -0.5 -40.0000017 -60 0 1 0 0
-# *loadtype 10 1
-
-# # MPC created
-# *createmark nodes 2 7118 7123
-# *createarray 2 1 1
-# *createdoublearray 12 1 1 1 1 1 1 1 1 1 1 1 1
-# *equationcreate 2 1 2 1 12 7136 1 -1 0
-# *createmark equations 1 1
-# *findmark equations 1 0 1 equations 0 0;
-
-# # Modified Dependent dof of Equation
-# *setvalue equations id=1 STATUS=2 dependentdof=3
-
-# # Attached attributes to Equation
-# *setvalue equations id=1 STATUS=2 3263=0
-# *mergehistorystate "" ""
-
-# # Modified WF 3 of Equation
-# *setvalue equations id=1 STATUS=2 ROW=0 independentcoeffs3= {1}
-# *setvalue equations id=1 STATUS=2 ROW=1 independentcoeffs3= {}
-
-# # Modified WF 3 of Equation
-# *setvalue equations id=1 STATUS=2 ROW=0 independentcoeffs3= {1}
-# *setvalue equations id=1 STATUS=2 ROW=1 independentcoeffs3= {-1}
-
-# # Modified WF 1 of Equation
-# *setvalue equations id=1 STATUS=2 ROW=0 independentcoeffs1= {}
-# *setvalue equations id=1 STATUS=2 ROW=1 independentcoeffs1= {1}
-
-# # Modified WF 1 of Equation
-# *setvalue equations id=1 STATUS=2 ROW=0 independentcoeffs1= {}
-# *setvalue equations id=1 STATUS=2 ROW=1 independentcoeffs1= {}
-
-# # Modified Dependent coeff of Equation
-# *setvalue equations id=1 STATUS=2 dependentcoeff=1
-# *createentity loadcols includeid=0 name="loadcol1"
-# *clearmark loadcols 1
-
-# # Renamed Load Collector from "loadcol1" to "MPC"
-# *setvalue loadcols id=4 name=MPC
-
-# # Moved equations into component "MPC"
-# *createmark equations 1 1
-# *movemark equations 1 "MPC"
+*clearmark elems $markid_elems
