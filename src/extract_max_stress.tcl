c90eb0224bd0b21e2455aed856e0688d44ed4d0e
WyattHo <wyatthoho@gmail.com>
2023-08-10 14:38:08 +0800

    refactor

--- src/extract_max_stress.tcl
+++ src/extract_max_stress.tcl
@@ -29,17 +29,51 @@ contour SetSelectionSet $set_idx
 client GetMeasureHandle measure 1
 page GetAnimatorHandle animator
 
-set filepath [model GetFileName]
-set idx_str [expr [string last "\\" $filepath] + 1]
-set idx_end [expr [string length $filepath]-5]
-set filenamepre [string range $filepath $idx_str $idx_end]
-set filenamemid "_maximum_stress_subcase"
-set loop_inc 30
+
+proc get_prename_of_csv {} {
+    set filepath [model GetFileName]
+    set idx_str [expr [string last "\\" $filepath] + 1]
+    set idx_end [expr [string length $filepath] - 5]
+    set filename_a [string range $filepath $idx_str $idx_end]
+    set filename_b "_maximum_stress_subcase"
+    return "$filename_a$filename_b"
+}
+
+
+proc initial_csv {prename subcase} {
+    set csv [open "$prename$subcase.csv" w+]
+    puts $csv "frequency,max_stress"
+    return $csv
+}
+
+
+proc iter_frames {loop_inc} {
+    set end_frame [animator GetEndFrame]
+    for {set frame_idx 0} {$frame_idx < $end_frame} {incr frame_idx} {
+        set angle [expr $frame_idx * $loop_inc]
+        if {$angle > 180} {
+            break
+        }
+        hwc animate frame [expr $frame_idx + 1]
+    }
+}
+
+
+proc get_maximum_stress_and_save_data {simu_label csv} {
+    set max_stress [measure GetMaximum scalar]
+    set idx_str [expr [string first = $simu_label] + 2]
+    set frequency [string range $simu_label $idx_str end]
+    puts $csv "$frequency,$max_stress"
+}
+
+
+set loop_inc 15
 set subcases [result GetSubcaseList "Base"]
+set prename [get_prename_of_csv]
+
 foreach subcase $subcases {
     result SetCurrentSubcase $subcase
-    set fp [open "$filenamepre$filenamemid$subcase.csv" w+]
-    puts $fp "frequency,max_stress"
+    set csv [initial_csv $prename $subcase]
 
     set simu_num [llength [result GetSimulationList $subcase]]
     for {set simu_idx 0} {$simu_idx < $simu_num} {incr simu_idx} {
@@ -51,21 +85,10 @@ foreach subcase $subcases {
         if {[string match Time* $simu_label]} {
             break
         }
-        
-        set idx_str [expr [string first = $simu_label] + 2]
-        set frequency [string range $simu_label $idx_str end]
-        set end_frame [animator GetEndFrame]
-        for {set frame_idx 0} {$frame_idx < $end_frame} {incr frame_idx} {
-            set angle [expr $frame_idx * $loop_inc]
-            if {$angle > 180} {
-                break
-            }
-            hwc animate frame [expr $frame_idx + 1]
-        }
-        set max_stress [measure GetMaximum scalar]
-        puts $fp "$frequency,$max_stress"
+        iter_frames $loop_inc
+        get_maximum_stress_and_save_data $simu_label $csv
     }
-    close $fp
+    close $csv
 }
 
 
