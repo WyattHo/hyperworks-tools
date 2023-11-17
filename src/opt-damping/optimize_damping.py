030c7cfa916c9dc0b0240f8af19a19f53af49eb5
WyattHo <wyatthoho@gmail.com>
2023-11-17 09:56:06 +0800

    feat: get peak response

--- src/opt-damping/optimize_damping.py
+++ src/opt-damping/optimize_damping.py
@@ -35,20 +35,30 @@ def retrieve_acceleration(config: dict):
     return subprocess.run(cmd, cwd=cwd, shell=True, capture_output=True)
 
 
-def calculate_error(config: dict):
+def get_peak_response(config: dict) -> list[float]:
     cwd = config['cwd']
     csv_name = config['model'] + '-subcase2.csv'
     target = config['target']
+    G = 9806.65
 
     csv_path = Path(cwd).joinpath(csv_name)
     df = pd.read_csv(csv_path)
+    peak_freq, peak_acc = 0, 0
+    for col in df.columns:
+        if not col.startswith('Time'):
+            max_acc = df[col].max()
+            max_idx = df[col].idxmax()
+            if max_acc > peak_acc:
+                peak_acc = max_acc
+                peak_freq = df['Time'].iloc[max_idx]
+    return [peak_freq, peak_acc / G]
 
 
 def main():
     config = read_configuration('config.json')
     # run_solver(config)
     # retrieve_acceleration(config)
-    calculate_error(config)
+    peak_freq, peak_acc = get_peak_response(config)
 
 
 if __name__ == '__main__':
